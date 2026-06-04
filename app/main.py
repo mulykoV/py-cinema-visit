3be77544905a0a786cf225e2906af43aa6ed14d4
Victor Shnaider <v.shnaider@jettdevelops.com>
2026-06-05 01:00:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-    customers: list,
-    hall_number: int,
-    cleaner: str,
-    movie: str
+    movie: str, 
+    customers: list, 
+    hall_number: int, 
+    cleaner: str
 ) -> None:
 
     customer_objects = [Customer(**c_dict) for c_dict in customers]
